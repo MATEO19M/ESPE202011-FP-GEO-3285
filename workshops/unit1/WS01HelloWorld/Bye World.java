83654e52b7e3e468a80c51f1fe1182b391881f61
MATEO19M <mnmartinez2@espe.edu.ec>
2021-01-08 14:26:02 -0500

    Workshop01 Hello World in three programming languages

---
+++ workshops/unit1/WS01HelloWorld/Bye World.java
@@ -0,0 +1,8 @@
+public class Bye World {
+
+    
+    public static void main(String[] args) {
+			System.out.println("Bye World from Mateo Martinez");
+			}
+			}
+			
---
+++ workshops/unit1/WS01HelloWorld/Hello World.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+     //printf() displays information
+       printf("Hello World from Mateo Martineez! =)");
+       return 0;
+
+}
